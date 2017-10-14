93fb14443f24c6e9fbe9d072cf57d8ba9b37aada
Jasmina_K <jasminakukunoska@gmail.com>
2017-10-14 22:32:36 +0200

    Сите фајлови

---
+++ jqueryZaNiR.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	$("#linkZaNajava").click(function){
+		$("#divZaNajava").show();
+	});
+});
+
+function show()
+{
+   $("#divZaNajava").fadeIn();
+   $("#divZaNajava").css({"visibility":"visible","display":"block"});
+}
